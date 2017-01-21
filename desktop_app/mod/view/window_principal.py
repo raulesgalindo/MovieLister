8b6e8e8ac2d69c7b9fffddd5fe9ed248826a88af
Raul Galindo <raulgalindorosales@gmail.com>
2017-01-20 22:25:44 -0600

    Add and remove elements from list
    python 3x is by reference :o

--- desktop_app/mod/view/window_principal.py
+++ desktop_app/mod/view/window_principal.py
@@ -1,6 +1,8 @@
 import tkinter
 from tkinter import *
 from tkinter import messagebox
+from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import showerror
 from ..logic.structures import *
 
 class window_principal:
@@ -16,13 +18,35 @@ class window_principal:
         frame = Frame(window)
         frame.pack()
         Lb1 = Listbox(frame)
-        listNodeFiles.append(node_file("lara","vel"))
-        for nodeFile in listNodeFiles:
-            Lb1.insert(7, nodeFile.fileName)
         Lb1.pack()
-        buttonOpen = Button(frame, text="Open",command = lambda: self.refresh_list_files(listNodeFiles,Lb1))
+        #buttonOpen = Button(frame, text="Open",command = lambda: self.refresh_list_files(listNodeFiles,Lb1))
+        buttonOpen = Button(frame, text="Open",command = lambda: self.load_file(listNodeFiles,Lb1))
         buttonOpen.pack( side = LEFT)
-        buttonRemove = Button(frame, text="Remove")
+        buttonRemove = Button(frame, text="Remove",command = lambda: self.deleteSelection(listNodeFiles,Lb1))
         buttonRemove.pack( side = LEFT )
     def refresh_list_files(self, listFiles, listUI):
-        messagebox.showinfo("Say Hello", "Hello World")
+        #messagebox.showinfo("Say Hello", "Hello World")
+        indiceArchivo = 0
+        listUI.delete(0,END)
+        for nodeFile in listFiles:
+            listUI.insert(indiceArchivo , nodeFile.fileName)
+            indiceArchivo = indiceArchivo + 1#Actualizamos el valor
+            listUI.update_idletasks()
+    def load_file(self,listFiles,listUI):
+        fname = askopenfilename(filetypes=(("Rar files", "*.rar"),
+                                           ("All files", "*.*") ))
+        if fname:
+            try:
+                listFiles.append(node_file(fname,fname))
+                self.refresh_list_files(listFiles, listUI)
+            except Exception as e: print (e)
+            return
+    def deleteSelection(self,listFiles,listUI) :
+        items = listUI.curselection()
+        pos = 0
+        for i in items :
+            idx = int(i) - pos
+            listUI.delete( idx,idx )
+            listFiles.pop(idx)    #eliminamos de la lista
+            pos = pos + 1
+        listUI.update_idletasks()
